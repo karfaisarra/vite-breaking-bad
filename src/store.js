eda6f000053b2fdd0768468cfaaa039047928339
karfaisarra <karfaisarra@gmail.com>
2022-11-14 16:29:01 +0100

    add store

--- src/store.js
+++ src/store.js
@@ -2,6 +2,5 @@ import { reactive } from 'vue'
 export const store = reactive({
     API_URL: 'https://www.breakingbadapi.com/api/characters',
     characters: null,
-    info: null,
     error: null,
 })
