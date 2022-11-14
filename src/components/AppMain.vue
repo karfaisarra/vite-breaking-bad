f26252e59254c0bfe392d22e2f9f92c88c003155
karfaisarra <karfaisarra@gmail.com>
2022-11-14 16:18:05 +0100

    add selectCategory

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,8 +1,10 @@
 <script>
+import SelectCategoy from './SelectCategoy.vue';
 import CharactersList from './CharactersList.vue'
 export default {
     name: 'AppMain',
     components: {
+        SelectCategoy,
         CharactersList,
     }
 }
@@ -10,11 +12,7 @@ export default {
 
 <template>
     <main>
-        <div class="selectCategory ">
-            <select name="" id="" class="p-2 rounded-3">
-                <option value="">Select category</option>
-            </select>
-        </div>
+        <SelectCategoy />
         <CharactersList />
     </main>
 </template>
---
+++ src/components/SelectCategoy.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'SelectCategory'
+}
+</script>
+
+<template>
+    <div class="container py-4">
+        <div class="selectCategory ">
+            <select name="" id="" class="p-2 rounded-3">
+                <option value="">Select category</option>
+            </select>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
