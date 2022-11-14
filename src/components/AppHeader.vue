5052238ff3e937aacffcbe0633257f27665181d7
karfaisarra <karfaisarra@gmail.com>
2022-11-14 15:59:11 +0100

    add header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header class="d-flex p-2">
+        <img width="80" src="../assets/img/Breaking_Bad_logo.svg" alt="">
+        <h1>Breaking Bad Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/variables' as *;
+
+h1 {
+    color: $bb-light;
+}
+</style>
